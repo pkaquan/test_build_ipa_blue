f678061ecbb287e0da3b8594bdaec5f3bcb78318
pkaquan <mam4ch333@gmail.com>
2025-03-24 19:45:25 +0700

    first commit

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,6 +1,7 @@
+import 'package:bluetooth/streambuild.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:get/get.dart';
 
 class Screen extends StatefulWidget {
   const Screen({super.key});
@@ -10,84 +11,56 @@ class Screen extends StatefulWidget {
 }
 
 class _ScreenState extends State<Screen> {
-  List<ScanResult> _devicesList = [];
-  bool isConnected = false;
-
-  final String ssid = "your_ssid"; // Thay thế bằng SSID của bạn
-  final String password = "your_password"; // Thay thế bằng password của bạn
-
-  @override
-  void initState() {
-    super.initState();
-    permissionBlue();
-  }
-
-  void permissionBlue() async {
-    Map<Permission, PermissionStatus> status =
-        await [
-          Permission.bluetooth,
-          Permission.bluetoothScan,
-          Permission.bluetoothConnect,
-        ].request();
-    if (status[Permission.bluetooth]!.isGranted &&
-        status[Permission.bluetoothScan]!.isGranted &&
-        status[Permission.bluetoothConnect]!.isGranted) {
-      print("Quyền đã được cấp");
-      startScan();
-    } 
-  }
-
-  Future startScan() async {
-    _devicesList.clear();
-    try {
-      await FlutterBluePlus.startScan(
-        timeout: Duration(seconds: 4),
-        continuousUpdates: true,
-      );
-      FlutterBluePlus.scanResults.listen((result) {
-        _devicesList = result;
-        if (mounted) {
-          setState(() {});
-        }
-      });
-      await FlutterBluePlus.stopScan();
-    } catch (e) {
-      e.toString();
-    }
-  }
-
-  // Gửi SSID và password
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Danh sách thiết bị Bluetooth")),
-      body: ListView.builder(
-        itemCount: _devicesList.length,
-        itemBuilder: (context, index) {
-          if (_devicesList[index].device.isConnected) {
-            setState(() {
-              isConnected = true;
-            });
-          } else if (_devicesList[index].device.isDisconnected) {
-            setState(() {
-              isConnected = false;
-            });
-          }
-          return ListTile(
-            title: Text(
-              _devicesList[index].device.platformName.isEmpty
-                  ? "Unknown Device"
-                  : _devicesList[index].device.platformName,
+      body: GetBuilder<Streambuild>(
+        init: Streambuild(),
+        builder: (Streambuild controller) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                StreamBuilder<List<ScanResult>>(
+                  stream: controller.scanResult,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Expanded(
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            final data = snapshot.data![index];
+                            return Card(
+                              elevation: 2,
+                              child: ListTile(
+                                title: Text(data.device.name),
+                                subtitle: Text(data.rssi.toString()),
+                                onTap:
+                                    () => controller.connectingDevice(data.device),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    } else {
+                      return Center(child: Text("Không tìm thấy thiết bị"));
+                    }
+                  },
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    controller.startScan();
+                    // await controller.disconnectDevice();
+                  },
+                  child: Text("SCAN"),
+                ),
+              ],
             ),
-            subtitle: Text(isConnected ? "Kết nối" : "Đã kế nối"),
-            onTap: () => _devicesList[index].device.connect(),
           );
         },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        label: Icon(Icons.search),
-        onPressed: startScan,
       ),
     );
   }

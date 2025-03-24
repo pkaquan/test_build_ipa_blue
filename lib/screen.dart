d87209da31b8bfb1a67ba91720e975c8f91ae53f
pkaquan <mam4ch333@gmail.com>
2025-03-24 20:57:48 +0700

    first commit

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,7 +1,6 @@
-import 'package:bluetooth/streambuild.dart';
+//
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:get/get.dart';
 
 class Screen extends StatefulWidget {
   const Screen({super.key});
@@ -11,56 +10,87 @@ class Screen extends StatefulWidget {
 }
 
 class _ScreenState extends State<Screen> {
+  final FlutterBlue blue = FlutterBlue.instance;
+  List<BluetoothDevice> listDevice = [];
+  bool isScanning = false;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void startScan() {
+    setState(() {
+      isScanning = false;
+      listDevice.clear();
+    });
+    blue.startScan(timeout: Duration(seconds: 5));
+    blue.scanResults.listen((result) {
+      for (ScanResult r in result) {
+        if (listDevice.contains(r.device)) {
+          setState(() {
+            listDevice.add(r.device);
+          });
+        }
+      }
+    });
+    blue.isScanning.listen((isScanning) {
+      setState(() {
+        this.isScanning = isScanning;
+      });
+    });
+  }
+
+  Future<void> connectToDevice(BluetoothDevice device) async {
+    try {
+      await device.connect();
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Đã kết nối với ${device.name}')));
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Lỗi kết nối: $e')));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Danh sách thiết bị Bluetooth")),
-      body: GetBuilder<Streambuild>(
-        init: Streambuild(),
-        builder: (Streambuild controller) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                StreamBuilder<List<ScanResult>>(
-                  stream: controller.scanResult,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Expanded(
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (context, index) {
-                            final data = snapshot.data![index];
-                            return Card(
-                              elevation: 2,
-                              child: ListTile(
-                                title: Text(data.device.name),
-                                subtitle: Text(data.rssi.toString()),
-                                onTap:
-                                    () => controller.connectingDevice(data.device),
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    } else {
-                      return Center(child: Text("Không tìm thấy thiết bị"));
-                    }
-                  },
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () async {
-                    controller.startScan();
-                    // await controller.disconnectDevice();
-                  },
-                  child: Text("SCAN"),
-                ),
-              ],
+      appBar: AppBar(
+        title: Text('Bluetooth'),
+        actions: [
+          IconButton(
+            onPressed: isScanning ? null : startScan,
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          if (isScanning)
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: LinearProgressIndicator(),
             ),
-          );
-        },
+          Expanded(
+            child: ListView.builder(
+              itemCount: listDevice.length,
+              itemBuilder: (context, index) {
+                BluetoothDevice device = listDevice[index];
+                return ListTile(
+                  title: Text(
+                    device.name.isEmpty ? "Thiết bị không tên" : device.name,
+                  ),
+                  leading: Text(device.id.toString()),
+                  trailing: ElevatedButton(
+                    onPressed: () => connectToDevice(device),
+                    child: Text("Kết nối"),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/streambuild.dart
+++ lib/streambuild.dart
@@ -1,30 +1,30 @@
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:get/get.dart';
-import 'package:permission_handler/permission_handler.dart';
+// import 'package:flutter_blue/flutter_blue.dart';
+// import 'package:get/get.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
-class Streambuild extends GetxController {
-  FlutterBlue blue = FlutterBlue.instance;
-  Future startScan() async {
-    if (await Permission.bluetoothScan.request().isGranted) {
-      if (await Permission.bluetoothConnect.request().isGranted) {
-        blue.startScan(timeout: Duration(seconds: 5));
-        blue.stopScan();
-      }
-    }
-  }
+// class Streambuild extends GetxController {
+//   FlutterBlue blue = FlutterBlue.instance;
+//   Future startScan() async {
+//     if (await Permission.bluetoothScan.request().isGranted) {
+//       if (await Permission.bluetoothConnect.request().isGranted) {
+//         blue.startScan(timeout: Duration(seconds: 5));
+//         blue.stopScan();
+//       }
+//     }
+//   }
 
-  Future<void> connectingDevice(BluetoothDevice device) async {
-    await device.connect(timeout: Duration(seconds: 15));
-    device.state.listen((isConnecting) {
-      if (isConnecting == BluetoothDeviceState.connecting) {
-        print("Đang kết nối");
-      } else if (isConnecting == BluetoothDeviceState.connected) {
-        print("Đã kết nối");
-      } else {
-        print("Chưa kết nối");
-      }
-    });
-  }
+//   Future<void> connectingDevice(BluetoothDevice device) async {
+//     await device.connect(timeout: Duration(seconds: 15));
+//     device.state.listen((isConnecting) {
+//       if (isConnecting == BluetoothDeviceState.connecting) {
+//         print("Đang kết nối");
+//       } else if (isConnecting == BluetoothDeviceState.connected) {
+//         print("Đã kết nối");
+//       } else {
+//         print("Chưa kết nối");
+//       }
+//     });
+//   }
 
-  Stream<List<ScanResult>> get scanResult => blue.scanResults;
-}
+//   Stream<List<ScanResult>> get scanResult => blue.scanResults;
+// }
